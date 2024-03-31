2475ad8d689936bcc9dd7bd4fdad8be55e2eb051
Dillon Miller <64083498+dillonAMiller@users.noreply.github.com>
2024-03-31 09:40:44 -0400

    login input

--- day 2.py
+++ day 2.py
@@ -1,9 +1,15 @@
-print("hello world")
-print("day two of coding")
-print("hello, " + input("What is your name?"))
-print("this is a test")
 # i want to ask for an input for username and password then have the user
 # type in a username and password and check the typed in variables against the stored
 # usernames and passwords
 username = input("What is your username?")
+password = input("What is your password?")
+
+print("Type your username and password")
+inputUsername = input("Username:")
+inputPassword = input("Password:")
+
+if(username == inputUsername
+   print("login successful!")
+   else)
+
 
